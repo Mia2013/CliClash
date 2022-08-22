56e96f7eab6194c550f611f7540a042f7fa55be5
Mia2013 <komlosnikolett@gmail.com>
2022-08-22 09:46:06 +0200

    add class Weapon

---
+++ Weapons/Weapon.js
@@ -0,0 +1,15 @@
+export default class Weapon {
+  name;
+  whoCanUse;
+  damage;
+  chanceOfSuccess;
+
+  constructor() {
+    this.name = "null";
+    this.whoCanUse = [];
+    this.damage = [];
+    this.chanceOfSuccess = "null";
+  }
+
+  
+}
